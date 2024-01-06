15cd31d98a5fcf169a9b81591580f82d9bedd8ff
imtiaz-risat <ahababimtiaz@iut-dhaka.edu>
2024-01-06 19:02:22 +0600

    Added Billing/Invoice generating system

---
+++ Billings/InvoiceGenerator.java
@@ -0,0 +1,9 @@
+public class InvoiceGenerator implements Billing {
+    @Override
+    public double generateInvoice(Order order) {
+        // Implementation to generate an invoice for an order
+        // Actual calculation needed
+        System.out.println("Invoice generated for the order");
+        return 0.0;
+    }
+}
